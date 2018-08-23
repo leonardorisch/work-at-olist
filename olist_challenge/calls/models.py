42feee187247f2b60bd6c499e2a2d15f7945be35
Leonardo <leo.mrisch@gmail.com>
2018-08-23 09:06:53 -0300

    add call models

--- olist_challenge/calls/models.py
+++ olist_challenge/calls/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Call(models.Model):
+    id = models.AutoField(primary_key=True)
+    call_id = models.PositiveIntegerField(db_index=True)
+    type = models.CharField(max_length=5)
+    timestamp = models.CharField(max_length=50)
+    class Meta:
+        abstract = True
+
+class StartCall(Call):
+    origin = models.CharField(max_length=15)
+    destination = models.CharField(max_length=15)
+
+class EndCall(Call):
+    pass
