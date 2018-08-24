556450243adfd7f144f0f0ee567502ec64ab70be
Leonardo <leo.mrisch@gmail.com>
2018-08-24 13:24:59 -0300

    create bills migrations/fixing based on bills dependency

--- olist_challenge/calls/models.py
+++ olist_challenge/calls/models.py
@@ -1,16 +1,15 @@
 from django.db import models
 
 class Call(models.Model):
-    id = models.AutoField(primary_key=True)
-    call_id = models.PositiveIntegerField(db_index=True)
-    type = models.CharField(max_length=5)
+    call_id = models.PositiveIntegerField()
     timestamp = models.CharField(max_length=50)
     class Meta:
         abstract = True
 
 class StartCall(Call):
-    origin = models.CharField(max_length=15)
+    origin = models.CharField(max_length=15, db_index=True)
     destination = models.CharField(max_length=15)
+    type = models.CharField(max_length=5, default="start")
 
 class EndCall(Call):
-    pass
+    type = models.CharField(max_length=5, default="end")
--- olist_challenge/olist_challenge/settings.py
+++ olist_challenge/olist_challenge/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'calls'
+    'calls',
+    'bills'
 ]
 
 MIDDLEWARE = [
